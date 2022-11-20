66dfe64b9547a1bcefd9d774796642a8dbf413de
tomast1337 <vycasnicolas@gmail.com>
2022-11-20 18:17:03 -0300

    Concerto no import

--- pythonGC/pythonGC/GL2Piramide.py
+++ pythonGC/pythonGC/GL2Piramide.py
@@ -8,11 +8,9 @@ from basicGL2 import BasicOpenGLApp
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-from colorstuff import HSL2RGB
+from pythonGC.color_util import HSL2RGB
 
 PI = 3.1415926535897932384626433832795
-
-
 class Piramide(BasicOpenGLApp):
     def __init__(self, full_screen=True):
         """
